bee40cd043d1e081906a93d3bce34baeb4d3e0a5
Matthias Pfefferle <pfefferle@gmail.com>
2017-04-28 23:10:25 +0200

    added language support

--- templates/admin.php
+++ templates/admin.php
@@ -7,26 +7,26 @@ $GLOBALS['tab'] = 'custom';
 
 ?>
 <div class="wrap">
-	<h2>OStatus</h2>
+	<h2><?php _e( 'OStatus', 'ostatus' ); ?></h2>
 
-	<p><strong>OStatus for WordPress turns your blog into a federated social network.
+	<p><strong><?php _e( 'OStatus for WordPress turns your blog into a federated social network.
 	This means you can share and talk to everyone using the OStatus protocol,
-	including users of Status.net and Identi.ca</strong></p>
+	including users of Status.net and Identi.ca', 'ostatus' ); ?></strong></p>
 
-	<p>Some Links:
+	<p><?php _e( 'Some Links:', 'ostatus' ); ?>
 		<ul>
-			<li><a href="http://ostatus.org" target="_blank">OStatus.org</a></li>
-			<li><a href="http://ostatus.org/2010/10/04/how-ostatus-enable-your-application" target="_blank">How to OStatus-enable Your Application</a></li>
-			<li><a href="http://wordpress.org/tags/ostatus-for-wordpress" target="_blank">Give us feedback</a></li>
+			<li><a href="https://www.w3.org/community/ostatus/" target="_blank"><?php _e( 'w3.org community page', 'ostatus' ); ?></a></li>
+			<li><a href="https://www.w3.org/community/ostatus/wiki/Howto" target="_blank"><?php _e( 'How to OStatus-enable Your Application', 'ostatus' ); ?></a></li>
+			<li><a href="https://github.com/pfefferle/wordpress-ostatus/issues" target="_blank"><?php _e( 'Give us feedback', 'ostatus' ); ?></a></li>
 		</ul>
 	</p>
 
-	<h3>Dependencies</h3>
+	<h3><?php _e( 'Dependencies', 'ostatus' ); ?></h3>
 
-	<p>OStatus is like a <em>Best of OpenWeb Standards</em> and so is this plugin.
+	<p><?php _e( 'OStatus is like a <em>Best of OpenWeb Standards</em> and so is this plugin.
 	If there is a plugin available that already implements one of these standards, we will use/support it.
-	If we are missing one, <a href="http://wordpress.org/tags/ostatus-for-wordpress" target="_blank">please let us know</a>.
-	The installation is a bit painful, but we think it's much more <em>open style</em> ;)</p>
+	If we are missing one, <a href="https://github.com/pfefferle/wordpress-ostatus/issues" target="_blank">please let us know</a>.
+	The installation is a bit painful, but we think it\'s much more <em>open style</em> ;)', 'ostatus' ); ?></p>
 <?php
 $plugins = array();
 
